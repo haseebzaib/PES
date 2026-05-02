a6e11ff95737042d452d2b890ef4a3130a068479
haseebzaib <hzaib76@gmail.com>
2026-05-02 13:42:26 +0500

    reduced loop delay

--- src/core/sensor_thread.cpp
+++ src/core/sensor_thread.cpp
@@ -37,7 +37,7 @@ namespace core
         {
             toggle_led();
             interfaces_loop();
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
         }
     }
 
